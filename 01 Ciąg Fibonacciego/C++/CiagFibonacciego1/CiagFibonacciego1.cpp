04cc68a0aeebd4503842ef3d43a75fd4698a1378
anndx <annadutqa@gmail.com>
2022-05-10 23:47:53 +0200

    Add '01 Ciag Fibonacciego' folder

---
+++ 01 Ciąg Fibonacciego/C++/CiagFibonacciego1/CiagFibonacciego1.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+using namespace std;
+int liczba_wyrazow;
+int ciag[100000];
+
+int main()
+{
+    cout << "Liczba wyrazow ciagu Fibonacciego: ";
+    cin >> liczba_wyrazow;
+
+    ciag[0] = 1;
+    ciag[1] = 1;
+
+    for (int i = 2; i < liczba_wyrazow; i++) {
+        ciag[i] = ciag[i - 2] + ciag[i - 1];
+    }
+
+    for (int i = 0; i < liczba_wyrazow; i++) {
+        cout << ciag[i] << endl;
+    }
+}
